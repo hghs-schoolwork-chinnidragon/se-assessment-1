3c1a1a817e53f69d1ba7c5b4c308438291df5d7a
chinnidragon <81355652+chinnidragon@users.noreply.github.com>
2025-06-25 11:10:13 +1000

    test

--- mthmenu.py
+++ mthmenu.py
@@ -1,13 +1,6 @@
 menu = input("what would you like to do? (1, 2, 3, 4)")
 
 if menu == 1:
-    dothis()
-if menu == 2:
-    dothat()
-if menu == 3:
-    dothose()
-if menu == 4:
-    dosomethingelse()
-else:
+    print("sigma")
     print("invalid")
 
