354b69ed2e82999ecef03c26ae2b4a4c5b205578
chinnidragon <81355652+chinnidragon@users.noreply.github.com>
2025-06-25 10:50:35 +1000

    menu

---
+++ mthmenu.py
@@ -0,0 +1,13 @@
+menu = input("what would you like to do? (1, 2, 3, 4)")
+
+if menu == 1:
+    dothis()
+if menu == 2:
+    dothat()
+if menu == 3:
+    dothose()
+if menu == 4:
+    dosomethingelse()
+else:
+    print("invalid")
+
