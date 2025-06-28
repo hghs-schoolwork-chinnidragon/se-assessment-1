cee7e3b56bce427ff90ea53dbe3204fe95a46a49
chinnidragon <81355652+chinnidragon@users.noreply.github.com>
2025-06-28 14:37:13 +1000

    load title for quiz

--- quizzing.py
+++ quizzing.py
@@ -1,7 +1,18 @@
 import json
 class Quiz():
-    def __init__(self, json):
-        self.json = json
+    def __init__(self, jsfile):
+        self.jsonf = jsfile
 
+    def set_json(self):
+        with open(self.jsonf, "r") as file:
+            info = json.load(file)
+    
     def set_title(self):
-        y = json
+        with open(self.jsonf, "r") as file:
+            info = json.load(file)
+        self.title = info['title']
+        print(self.title)
+
+
+roygbiv = Quiz("quiz.json")
+roygbiv.set_title()
