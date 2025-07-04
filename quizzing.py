f064c4b6da8cc76e30680d23a90dc244e6305116
chinnidragon <81355652+chinnidragon@users.noreply.github.com>
2025-07-04 10:41:06 +1000

    remove global variable

--- quizzing.py
+++ quizzing.py
@@ -1,48 +1,44 @@
 import json
 import random
 
-def load_quinfo(filename):
-    with open(filename, "r") as file:
-        global info 
-        info = json.load(file)
 
-class Quiz():
+class Quiz:
     def __init__(self, jsfile):
-        self.jsonf = jsfile
-    
+        self.__jsonf = jsfile
+        with open(jsfile, "r") as file:
+            self.__info = json.load(file)
+        print(self.__info)
     def load_title(self):
-        load_quinfo(self.jsonf)
-        self.title = info['title']
+        self.__title = self.__info['title']
         # print(self.title)
 
-    def load_qs(self, numq):
-        load_quinfo(self.jsonf)
-        self.questions = []
-        self.answers = []
-        self.potentans  = []
+    def load_qs(self):
+        self.__questions = []
+        self.__answers = []
+        self.__potentans  = []
         for i in range(1, numq+1):
-            self.questions.append(info['questions'][f'Question {i}'][0])
-            self.answers.append(info['questions'][f'Question {i}'][1])
-            self.potentans.append(info['questions'][f'Question {i}'][2])
+            self.__questions.append(info['questions'][f'Question {i}'][0])
+            self.__answers.append(info['questions'][f'Question {i}'][1])
+            self.__potentans.append(info['questions'][f'Question {i}'][2])
 
     def run_quiz(self):
-        print(f'Welcome to the {self.title} quiz!')
+        print(f'Welcome to the {self.__title} quiz!')
         question_number = 0
         score = 0
         # needs to generate in random order + not show the same options (maybe just display all?)
-        for q in range(0, len(self.questions)):
+        for q in range(0, len(self.__questions)):
             print(f"Question {question_number+1}:")
-            print(f"""{self.questions[question_number]['question']}
-* {self.potentans[question_number]['potential answer'][random.randint(0, 5)]}
-* {self.potentans[question_number]['potential answer'][random.randint(0, 5)]}
-* {self.potentans[question_number]['potential answer'][random.randint(0, 5)]}
-* {self.answers[question_number]['correct answer']}""")
+            print(f"""{self.__questions[question_number]['question']}
+* {self.__potentans[question_number]['potential answer'][random.randint(0, 5)]}
+* {self.__potentans[question_number]['potential answer'][random.randint(0, 5)]}
+* {self.__potentans[question_number]['potential answer'][random.randint(0, 5)]}
+* {self.__answers[question_number]['correct answer']}""")
             playerans = input("")
-            if playerans == {str(self.answers[question_number]['correct answer'])}:
+            if playerans == {str(self.__answers[question_number]['correct answer'])}:
                 score += 1
                 print(score)
             else:
-                print(f"Sorry! The correct answer is {self.answers[question_number]['correct answer']}")
+                print(f"Sorry! The correct answer is {self.__answers[question_number]['correct answer']}")
             question_number+= 1
     #how can i return them as 2 seperate numbers not a tuple
         return score, question_number
@@ -53,6 +49,6 @@ class Quiz():
 
 # need to make the Quiz as its own module and put these in a different file
 roygbiv = Quiz("quiz.json")
-roygbiv.load_title()
-roygbiv.load_qs(2)
-roygbiv.score(roygbiv.run_quiz())
+# roygbiv.load_title()
+# roygbiv.load_qs(2)
+# roygbiv.score(roygbiv.run_quiz())
