0d97c7b0dcb973504b05283d146e75a9117e4a6a
chinnidragon <81355652+chinnidragon@users.noreply.github.com>
2025-07-09 17:40:58 +1000

    add more questions to the quiz, briefly start working on a class for images

--- layerimage.py
+++ layerimage.py
@@ -6,6 +6,22 @@ window = tk.Tk()
 # chair = tk.PhotoImage(file="images/chair.png")
 # print(crimson.width()/2, crimson.height()/2)
 # crimson = crimson.resize((517, 600))
+class Avatars:
+    def __init__(self, pathToBase, pathToHair, pathToMouth, pathToAccesory):
+        self.__base = pathToBase
+        self.__hair = pathToHair
+        self.__mouth = pathToMouth
+        self.__accessories = pathToAccesory
+        
+    def createAvatar(self):
+        #still working on this!!
+        print("Hello World girl idk")
+
+def resize(height):
+    width = height*517/600
+    return width
+
+# print(resize(600))
 
 crimson = Image.open("images/crimson.png")
 crimson = crimson.resize((517, 600))
@@ -13,18 +29,8 @@ chair = Image.open("images/chair.png")
 chair = chair.resize((517, 600))
 crimson.paste(chair, (0, 0), chair)
 
-
 crimson =  ImageTk.PhotoImage(crimson)
 test = tk.Label(window, image=crimson)
 test.pack()
-# hair = tk.Label(window, image=chair)
-# hair.pack(padx=0,pady=0)
-# chair = chair.resize(chair.width/2, chair.height/2)
 
-# canvas = tk.Canvas(window, width=crimson.width)
-# canvas.place(x=0, y=0)
-# canvas.create_image(0, 0, anchor="nw", image=crimson)
-# canvas.place(x=0, y=0)
-# canvas.create_image(0, 0, anchor="nw", image=chair)
-# canvas.place(x=0, y=0)
 window.mainloop()
