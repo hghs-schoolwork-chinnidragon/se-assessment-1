cf6db8cf86bdae8179d8fcdaa1fa2448afae48a8
chinnidragon <81355652+chinnidragon@users.noreply.github.com>
2025-07-22 20:30:35 +1000

    add HSV info

--- mainprogram.py
+++ mainprogram.py
@@ -9,5 +9,5 @@ class Menu:
         print(quizzes[0].getTitle(), quizzes[0].getImage())
 q_whatColourIsThat = quiz.Quiz("whatColourIsThat.json")
 q_allAboutHSV = quiz.Quiz("allAboutHSV.json")
-q_whatColourIsThat.run()
+# q_whatColourIsThat.run()
 q_allAboutHSV.run()
