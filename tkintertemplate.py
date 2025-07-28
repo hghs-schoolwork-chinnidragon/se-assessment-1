87fa7101566bf6ca98044e24b489de9626daa83f
chinnidragon <81355652+chinnidragon@users.noreply.github.com>
2025-07-28 10:48:20 +1000

    question template wip 1

--- tkintertemplate.py
+++ tkintertemplate.py
@@ -2,9 +2,13 @@ import tkinter as tk
 from PIL import Image, ImageTk
 from quiz import Quiz
 
-class QuestionTemplate(Quiz):
-    def __init__(self):
-        pass
+class QuestionTemplate():
+    def __init__(self, tkinterwindow,  questionTitle, questionOptions):
+        self.__questionTitle = questionTitle
+        self.__questionOptions = questionOptions
+        self.__tkinterWindow = tkinterwindow
     def createQuestionWindow(self):
-        tk_questionTitle = tk.Label(window, text=f"{self.__questions}"Aria)
+        tk_questionTitle = tk.Label(self.__tkinterwindow, text=f"{self.__questionTitle}", font=("Arial", 60))
+        tk_questionTitle.grid()
+
         
