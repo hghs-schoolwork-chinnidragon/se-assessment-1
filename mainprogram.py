6a10dd698594d96981fd49c8edfb8161a2278e9e
chinnidragon <81355652+chinnidragon@users.noreply.github.com>
2025-08-08 19:31:49 +1000

    conflict 2

--- mainprogram.py
+++ mainprogram.py
@@ -1,34 +1,34 @@
 import quizmodule
 import tkinter as tk
 from PIL import Image, ImageTk
-class Menu:
-    # @staticmethod
-    # def createwindow():
-    #     window = tk.Tk()
+# class Menu:
+#     # @staticmethod
+#     # def createwindow():
+#     #     window = tk.Tk()
 
-    def __init__(self, quizzes):
-        window = tk.Tk()
-        window.geometry("1440x1024")
-        quizWidgets = []
-        for i in range(0, len(quizzes)):
-            quizWidgetImage = Image.open(f"{quizzes[i].getImage()}")
-            quizWidgetImage = quizWidgetImage.resize((round(quizWidgetImage.width/4), round(quizWidgetImage.height/4)))
-            quizWidgetImage = ImageTk.PhotoImage(quizWidgetImage)
-            t = quizzes[i].getTitle()
-            #need to resize images
-            def onClick():
-                return t
-            quizWidget = tk.Button(window, text=f"{t}", image=f"{quizWidgetImage}", compound="bottom", font=("Arial", 36), wraplength="500", command=onClick)
-            quizWidgets.append(quizWidget)
+#     def __init__(self, quizzes):
+#         window = tk.Tk()
+#         window.geometry("1440x1024")
+#         quizWidgets = []
+#         for i in range(0, len(quizzes)):
+#             quizWidgetImage = Image.open(f"{quizzes[i].getImage()}")
+#             quizWidgetImage = quizWidgetImage.resize((round(quizWidgetImage.width/4), round(quizWidgetImage.height/4)))
+#             quizWidgetImage = ImageTk.PhotoImage(quizWidgetImage)
+#             t = quizzes[i].getTitle()
+#             #need to resize images
+#             def onClick():
+#                 return t
+#             quizWidget = tk.Button(window, text=f"{t}", image=f"{quizWidgetImage}", compound="bottom", font=("Arial", 36), wraplength="500", command=onClick)
+#             quizWidgets.append(quizWidget)
             
-        for i in (quizWidgets):
-            for j in range(2, 8):
-                i.grid(
-                    row=j,
-                    column=j
-                )
+#         for i in (quizWidgets):
+#             for j in range(2, 8):
+#                 i.grid(
+#                     row=j,
+#                     column=j
+#                 )
 
-        window.mainloop()
+#         window.mainloop()
     
 
 
